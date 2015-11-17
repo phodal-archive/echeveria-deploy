44a735acf10db050ec6ef14022543a6b240adcee
Fengda HUANG <h@phodal.com>
2015-11-17 09:00:49 +0800

    remove comments

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,15 @@
-/*
- * assemble-examples <https://github.com/assemble/assemble-examples>
- *
- * Copyright (c) 2014 Jon Schlinkert, Brian Woodward, contributors.
- * Licensed under the MIT license.
- */
-
 var _ = require('lodash');
 var path = require('path');
 
 module.exports = function(grunt) {
   'use strict';
-
-
   var recipeTemplate = grunt.file.read('./templates/blog/blog.hbs');
 
-// expand the data files and loop over each filepath
   var pages = _.flatten(_.map(grunt.file.expand('./content/*.json'), function(filepath) {
     var data = grunt.file.readJSON(filepath);
-
-    // create a 'page' object to add to the 'pages' collection
     return {
-      // the filename will determine how the page is named later
       filename: path.basename(filepath, path.extname(filepath)),
-      // the data from the json file
       data: data,
-      // add the recipe template as the page content
       content: recipeTemplate
     };
   }));
@@ -82,14 +67,12 @@ module.exports = function(grunt) {
     }
   });
 
-  // Load the Assemble plugin.
   grunt.loadNpmTasks('assemble');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-connect');
 
-  // The default task to run with the `grunt` command.
   grunt.registerTask('dev', ['default', 'connect:server', 'watch:site']);
   grunt.registerTask('default', ['clean', 'assemble', 'copy']);
 };
